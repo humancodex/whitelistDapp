909b679f946552bfc4c3bfb64a39ac7de88d9916
Juan Sebastian <sebastian.carvajal@algoritmo.co>
2022-11-08 13:07:37 -0300

    contract deployed successfully!

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -2,8 +2,20 @@ const {ethers} = require("hardhat")
 
 async function main(){
   const whiteListContract = await ethers.getContractFactory("Whitelist");
-
+//create a instance of the contract
   const deployedWhitelistContract = await whiteListContract.deploy(10);
-
+//value of the constructor arguments 
    await deployedWhitelistContract.deployed();
+//deploy and print the address
+   
+
+  console.log("Whitelist contract address", deployedWhitelistContract.address);
 }
+
+main()
+.then(() => { process.exit(0)})
+.catch((error)=>{
+ console.error(error);
+ process.exit(1);
+ //state wasn't successfully 
+});
